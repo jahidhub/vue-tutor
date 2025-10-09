b924654ac555155a29825d27b3dfbaf48e88275d
SohanFlick <sohan.flick@gmail.com>
2025-10-09 20:55:41 +0600

    null

--- optionApiToDo/src/componants/Layout.vue
+++ optionApiToDo/src/componants/Layout.vue
@@ -1,5 +1,13 @@
 <script>
+import Model from "@/componants/Model.vue";
+import ToDoCard from "@/componants/ToDoCard.vue";
+
 export default {
+  components: {
+    Model,
+    ToDoCard,
+  },
+
   data() {
     return {
       todo: [
@@ -83,85 +91,9 @@ export default {
 
   <div class="max-w-7xl my-5 mx-auto">
     <div class="card-wrapper gap-4 grid md:grid-cols-2 lg:grid-cols-3 mx-5">
-      <div class="card p-4 bg-teal-500" v-for="task in todo">
-        <h3 class="text-white bg-black px-3 py-1 rounded inline-block">
-          ID : {{ task.id }}
-        </h3>
-        <div class="card-head flex justify-between items-center mt-4">
-          <h4 class="w-2/3 wrap-break-word text-white text-lg font-bold capitalize">
-            {{ task.title }}
-          </h4>
-          <div class="w-1/3 text-end">
-            <button
-              class="bg-white px-3 text-sm rounded text-red-600 cursor-pointer"
-              @click="removeTask(task.id)"
-            >
-              remove
-            </button>
-          </div>
-        </div>
-        <div class="card-body pt-4 text-white">
-          <p>
-            {{ task.description }}
-          </p>
-        </div>
-      </div>
+      <ToDoCard />
     </div>
-
-    <div
-      class="model-wrapper fixed inset-0 flex items-center justify-center bg-black/30"
-      v-if="showModel"
-    >
-      <div class="model md:w-[500px]">
-        <div class="p-4 bg-gray-500">
-          <div class="model-head flex justify-between items-center">
-            <h4 class="w-2/3 wrap-break-word text-white text-lg font-bold">
-              Add Task
-            </h4>
-            <div class="w-1/3 text-end">
-              <button
-                @click="showModel = false"
-                class="bg-white px-2 text-sm rounded text-black cursor-pointer"
-              >
-                X
-              </button>
-            </div>
-          </div>
-          <div class="model-body pt-4 text-white flex flex-col">
-            <label class="mb-3" for="title">Title</label>
-            <input
-              class="px-3 py-1 border bg-teal-500 text-white"
-              type="text"
-              id="title"
-              v-model="taskForm.title"
-              placeholder="Enter title"
-            />
-            <label class="mb-3 mt-5" for="description">Description</label>
-            <textarea
-              class="px-3 py-1 border bg-teal-500 text-white"
-              type="text"
-              id="description"
-              v-model="taskForm.description"
-              placeholder="Enter description"
-            />
-          </div>
-          <div class="gap-4 flex justify-end mt-5">
-            <button
-              @click="showModel = false"
-              class="bg-white px-3 text-lg rounded text-red-600 cursor-pointer"
-            >
-              close
-            </button>
-            <button
-              @click="addTask"
-              class="bg-white px-3 text-lg rounded text-green-600 cursor-pointer"
-            >
-              add
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Model />
   </div>
 </template>
 
