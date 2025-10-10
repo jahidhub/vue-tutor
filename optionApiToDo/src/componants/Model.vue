3071b38a5632070132d15df0c871a5f54e59d45b
jahid-hassan <jahidhassan9006@gmail.com>
2025-10-10 23:50:50 +0600

    data

---
+++ optionApiToDo/src/componants/Model.vue
@@ -0,0 +1,70 @@
+<script>
+export default {
+  emits: ["closeModel"],
+  data() {
+    return {
+      taskForm: {
+        title: "",
+        description: "",
+      },
+    };
+  },
+};
+</script>
+
+<template>
+  <div
+    class="model-wrapper fixed inset-0 flex items-center justify-center bg-black/30"
+  >
+    <div class="model md:w-[500px]">
+      <div class="p-4 bg-gray-500">
+        <div class="model-head flex justify-between items-center">
+          <h4 class="w-2/3 wrap-break-word text-white text-lg font-bold">
+            Add Task
+          </h4>
+          <div class="w-1/3 text-end">
+            <button
+              class="bg-white px-2 text-sm rounded text-black cursor-pointer"
+              @click="$emit('closeModel')"
+            >
+              X
+            </button>
+          </div>
+        </div>
+        <div class="model-body pt-4 text-white flex flex-col">
+          <label class="mb-3" for="title">Title</label>
+          <input
+            class="px-3 py-1 border bg-teal-500 text-white"
+            type="text"
+            id="title"
+            v-model="taskForm.title"
+            placeholder="Enter title"
+          />
+          <label class="mb-3 mt-5" for="description">Description</label>
+          <textarea
+            class="px-3 py-1 border bg-teal-500 text-white"
+            type="text"
+            id="description"
+            v-model="taskForm.description"
+            placeholder="Enter description"
+          />
+        </div>
+        <div class="gap-4 flex justify-end mt-5">
+          <button
+            @click="$emit('closeModel')"
+            class="bg-white px-3 text-lg rounded text-red-600 cursor-pointer"
+          >
+            close
+          </button>
+          <button
+            @click="addTask"
+            class="bg-white px-3 text-lg rounded text-green-600 cursor-pointer"
+          >
+            add
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style></style>
---
+++ optionApiToDo/src/componants/ToDoCard.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  props: {
+    task: Object,
+  },
+};
+</script>
+
+<template>
+  <div class="card p-4 bg-teal-500">
+    <h3 class="text-white bg-black px-3 py-1 rounded inline-block">
+      ID : {{ task.id }}
+    </h3>
+    <div class="card-head flex justify-between items-center mt-4">
+      <h4 class="w-2/3 wrap-break-word text-white text-lg font-bold capitalize">
+        {{ task.title }}
+      </h4>
+      <div class="w-1/3 text-end">
+        <button
+          class="bg-white px-3 text-sm rounded text-red-600 cursor-pointer"
+          @click="removeTask(task.id)"
+        >
+          remove
+        </button>
+      </div>
+    </div>
+    <div class="card-body pt-4 text-white">
+      <p>
+        {{ task.description }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<style lang=""></style>
