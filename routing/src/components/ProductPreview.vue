aec362824b318da5d765c18cc93a1cefd5ab0ec1
SohanFlick <sohan.flick@gmail.com>
2025-11-27 20:55:00 +0600

    null

--- routing/src/components/ProductPreview.vue
+++ routing/src/components/ProductPreview.vue
@@ -1,11 +1,33 @@
+<script>
+import { products } from "../../constants/data";
+
+export default {
+  data() {
+    return {
+      products,
+    };
+  },
+
+  mounted() {
+    const selectedProduct = this.products.find(
+      (p) => p.slug === this.$route.params.productSlug
+    );
+
+    // console.log("Found product:", product);
+
+    this.products = selectedProduct;
+  },
+};
+</script>
+
 <template>
   <div>
-    <h1>This is Products preview</h1>
-
-    {{ $route.params }}
+    <div class="container">
+      <h1>{{ products.name }}</h1>
+      <h5>{{ products.price }}</h5>
+      <p>{{ products.description }}</p>
+    </div>
   </div>
 </template>
-<script>
-export default {};
-</script>
+
 <style></style>
